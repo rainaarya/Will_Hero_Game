f873e91635262eef657134c978498c32fab24db4
Ashwin <ashwin20037@iiitd.ac.in>
2021-12-07 21:39:55 +0530

    Ashwin's Commit

--- src/main/java/willhero/will_hero_game/Controller.java
+++ src/main/java/willhero/will_hero_game/Controller.java
@@ -1,13 +1,22 @@
 package willhero.will_hero_game;
 
 import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 
 public class Controller implements Initializable {
 
@@ -19,6 +28,32 @@ public class Controller implements Initializable {
     private ImageView orc;
     @FXML
     private ImageView island;
+    @FXML
+    private CheckBox Music;
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+
+    public void switchToMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSettings(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 
     @Override
@@ -44,6 +79,14 @@ public class Controller implements Initializable {
         tt3.play();
 
 
+    }
+    public void PlayPauseMusic(ActionEvent event){
+        if(Music.isSelected()){
+            System.out.println("ON");
+        }
+        else{
+            System.out.println("OFF");
+        }
     }
 }
 
--- src/main/java/willhero/will_hero_game/Main.java
+++ src/main/java/willhero/will_hero_game/Main.java
@@ -9,12 +9,17 @@ import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainMenu.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage)  {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainMenu.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Will Hero");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
